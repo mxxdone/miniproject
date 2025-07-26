136eaec9c2f11d5fa9d8e0a1ddf5570fa844caa1
mxxdone <mxxdone@gmail.com>
2025-07-26 11:32:43 +0900

    style:	limit content to 3 lines and add bottom spacing

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -25,9 +25,12 @@ watch(
     <v-row>
       <v-col v-for="post in postsStore.posts" :key="post.id" cols="12" sm="6" md="4">
         <RouterLink :to="`/posts/${post.id}`" class="text-decoration-none">
-          <v-card>
-            <v-card-title>{{ post.title }}</v-card-title>
-            <v-card-text>{{ post.content }}</v-card-text>
+          <v-card class="h-100 d-flex flex-column justify-space-between">
+            <div>
+              <v-card-title>{{ post.title }}</v-card-title>
+              <v-card-text class="content-truncate pb-10">{{ post.content }}</v-card-text>
+              <div style="height: 10px"></div>
+            </div>
           </v-card>
         </RouterLink>
       </v-col>
@@ -42,3 +45,16 @@ watch(
     </v-row>
   </v-container>
 </template>
+<style scoped>
+.content-truncate {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: normal;
+  word-break: break-word;
+  line-height: 1.5;
+  max-height: calc(1.5em * 3);
+}
+</style>
