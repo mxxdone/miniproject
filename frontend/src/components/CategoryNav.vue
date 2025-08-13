2a81e2839fb4e3c55bd4a59160d9cf571298a380
mxxdone <mxxdone@gmail.com>
2025-08-14 03:42:37 +0900

    feat: 네비바 폰트 크기 조절, 토글 버튼 제거

--- frontend/src/components/CategoryNav.vue
+++ frontend/src/components/CategoryNav.vue
@@ -1,63 +1,68 @@
 <script setup>
-import { onMounted } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useCategoriesStore } from '@/stores/categories.js'
+import { usePostsStore } from '@/stores/posts'
 import { useRoute } from 'vue-router'
 
 const categoriesStore = useCategoriesStore()
-const route = useRoute() // route 객체 이용, 현재 경로 정보 사용.
+const postsStore = usePostsStore()
+const route = useRoute()
 
 onMounted(() => {
   categoriesStore.fetchCategories()
 })
+
+// 현재 활성화된 카테고리 ID
+const activeCategoryId = computed(() => {
+  if (route.name === 'postDetail' && postsStore.currentPost) {
+    return postsStore.currentPost.categoryId
+  }
+  return route.query.category ? Number(route.query.category) : null
+})
 </script>
 
 <template>
-  <v-list density="compact">
+  <v-list nav density="compact">
     <v-list-item
       prepend-icon="mdi-home"
       title="전체 게시글"
       to="/"
       exact
-      :active="!route.query.category"
-    ></v-list-item>
+      :active="!activeCategoryId && route.name === 'home'"
+    />
 
     <template v-for="category in categoriesStore.categories" :key="category.id">
-      <template v-if="category.name === 'About Me'">
-        <v-list-item
-          :prepend-icon="'mdi-account-box-outline'"
-          :title="category.name"
-          to="/about"
-        ></v-list-item>
-      </template>
+      <v-list-item
+        v-if="category.name === 'About Me'"
+        prepend-icon="mdi-account-box-outline"
+        to="/about"
+        :active="route.name === 'aboutMe'"
+      >
+        <v-list-item-title> About Me </v-list-item-title>
+      </v-list-item>
 
-      <template v-else>
+      <div v-else>
         <v-list-item
-          v-if="!category.children || category.children.length === 0"
-          :prepend-icon="'mdi-folder-outline'"
-          :title="category.name"
+          :prepend-icon="'mdi-folder-open-outline'"
           :to="`/?category=${category.id}`"
-          :active="Number(route.query.category) === category.id"
-        ></v-list-item>
+          :active="activeCategoryId === category.id"
+        >
+          <v-list-item-title class="text-subtitle-1 font-weight-medium">
+            {{ category.name }}
+          </v-list-item-title>
+        </v-list-item>
 
-        <v-list-group v-else :value="category.name">
-          <template v-slot:activator="{ props }">
-            <v-list-item
-              v-bind="props"
-              :prepend-icon="'mdi-folder-open-outline'"
-              :title="category.name"
-            ></v-list-item>
-          </template>
-
-          <v-list-item
-            v-for="child in category.children"
-            :key="child.id"
-            :title="child.name"
-            :to="`/?category=${child.id}`"
-            :active="Number(route.query.category) === child.id"
-            class="pl-8"
-          ></v-list-item>
-        </v-list-group>
-      </template>
+        <v-list-item
+          v-for="child in category.children"
+          :key="child.id"
+          :to="`/?category=${child.id}`"
+          :active="activeCategoryId === child.id"
+        >
+          <v-list-item-title class="ms-6">
+            {{ child.name }}
+          </v-list-item-title>
+        </v-list-item>
+      </div>
     </template>
   </v-list>
 </template>
