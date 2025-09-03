99a06c6d40427fe342cb011b1e8b646920cbfcb3
mxxdone <mxxdone@gmail.com>
2025-09-03 19:00:07 +0900

    fix: postId 역직렬화 오류로 인한 댓글 등록 실패 수정

--- backend/src/main/java/com/mxxdone/miniproject/dto/comment/CommentSaveRequestDto.java
+++ backend/src/main/java/com/mxxdone/miniproject/dto/comment/CommentSaveRequestDto.java
@@ -3,7 +3,7 @@ package com.mxxdone.miniproject.dto.comment;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.mxxdone.miniproject.config.XssSanitizer;
 
-public record CommentSaveRequestDto(@JsonDeserialize(using = XssSanitizer.class) Long postId,
+public record CommentSaveRequestDto(Long postId,
                                     @JsonDeserialize(using = XssSanitizer.class) String content,
-                                    @JsonDeserialize(using = XssSanitizer.class) Long parentId) {
+                                    Long parentId) {
 }
