22939e3edff9badbb255bae4b5680fbcf1c8e751
mxxdone <mxxdone@gmail.com>
2025-09-20 18:32:32 +0900

    feat: 백엔드 부모, 자식 댓글 전부 삭제될시 미노출. 댓글 조회 JPQL 쿼리 QueryDSL 적용

--- backend/src/main/java/com/mxxdone/miniproject/repository/CommentRepository.java
+++ backend/src/main/java/com/mxxdone/miniproject/repository/CommentRepository.java
@@ -2,10 +2,6 @@ package com.mxxdone.miniproject.repository;
 
 import com.mxxdone.miniproject.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
 }
