29436bfb608add39a25ca66752f5a95ec3aa3622
mxxdone <mxxdone@gmail.com>
2025-10-03 21:31:40 +0900

    feat: Open API Specification 추가

--- backend/src/main/java/com/mxxdone/miniproject/config/SecurityConfig.java
+++ backend/src/main/java/com/mxxdone/miniproject/config/SecurityConfig.java
@@ -12,8 +12,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -52,6 +50,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/v1/comments/**").permitAll()
                         // 게스트 댓글 삭제 허용
                         .requestMatchers(HttpMethod.DELETE, "/api/v1/comments/**").permitAll()
+                        // OAS(Open API Specification) 관련 접근 허용
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
                         //그 외 모든 요청은 인증 사용자만 접근 가능
                         .anyRequest().authenticated()
                 )
