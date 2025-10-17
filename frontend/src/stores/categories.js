07c54f0342c3f628fc1d4ecb51a9211df486d420
mxxdone <mxxdone@gmail.com>
2025-10-17 11:04:38 +0900

    refactor: 카테고리 중복 호출 방지

--- frontend/src/stores/categories.js
+++ frontend/src/stores/categories.js
@@ -3,15 +3,30 @@ import { ref } from 'vue'
 import apiClient from '@/api'
 
 export const useCategoriesStore = defineStore('categories', () => {
-  const categories = ref({})
+  const categories = ref([])
+  const isLoaded = ref(false)
+  const isLoading = ref(false)
 
   async function fetchCategories() {
+    if (isLoaded.value || isLoading.value) {
+      return // // 이미 카테고리 로드된 경우 API 호출 안 함
+    }
+
+    isLoading.value = true
+
     try {
       const response = await apiClient.get('http://localhost:8080/api/v1/categories')
       categories.value = response.data
+      isLoaded.value = true
     } catch (error) {
       console.error('카테고리를 불러오는 중 오류가 발생했습니다: ', error)
+    } finally {
+      isLoading.value = false
     }
   }
-  return { categories, fetchCategories }
-})
+  return {
+    categories,
+    isLoaded,
+    isLoading,
+    fetchCategories
+  }})
