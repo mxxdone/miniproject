48bdae0fdc77c451160c2872dac3f46199fdc18b
mxxdone <mxxdone@gmail.com>
2025-11-12 19:05:31 +0900

    chore: scp-action 경로 수정

--- backend/src/main/java/com/mxxdone/miniproject/MiniprojectApplication.java
+++ backend/src/main/java/com/mxxdone/miniproject/MiniprojectApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MiniprojectApplication {
 	public static void main(String[] args) {
-        // fix db url trigger
 		SpringApplication.run(MiniprojectApplication.class, args);
 	}
 
