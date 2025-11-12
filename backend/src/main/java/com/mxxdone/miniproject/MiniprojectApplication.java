e9dda15e0303e88cb824f2a8075975f29fb2ea03
mxxdone <mxxdone@gmail.com>
2025-11-12 16:42:16 +0900

    chore: 백엔드 배포를 위한 임시 수정

--- backend/src/main/java/com/mxxdone/miniproject/MiniprojectApplication.java
+++ backend/src/main/java/com/mxxdone/miniproject/MiniprojectApplication.java
@@ -2,11 +2,10 @@ package com.mxxdone.miniproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class MiniprojectApplication {
-
+    // create table trigger
 	public static void main(String[] args) {
 		SpringApplication.run(MiniprojectApplication.class, args);
 	}
